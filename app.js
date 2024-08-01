ea956ffdf23b524e99c127ad4265795cb01c423d
Léo Alves <leolorena.alves@gmail.com>
2024-07-31 23:22:53 -0300

    new commits

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const servidor = express();
 
+servidor.use(express.json())
+
 servidor.get('/helloworld', (req, resp) => {
   resp.send('Hello World')   //Código do EndPoint
 })
@@ -42,6 +44,27 @@ servidor.get('/mensagem/ola', (req, resp) => {
   let pessoa = req.query.nome ?? '!!!';
 
   resp.send('Olá ' + pessoa)
+})
+
+servidor.post('/media', (req, resp) => {
+  let n1 = req.body.nota1
+  let n2 = req.body.nota2
+  let n3 = req.body.nota3
+
+  let media = (n1 + n2 + n3) / 3
+
+  resp.send('Sua media atual é ' + media)
+})
+ 
+servidor.post('/dobros', (req, resp) => {
+  let nums = req.body.numeros
+  let nums2 = []
+
+  for (let i = 0; i < nums.length; i++) {
+    nums2[i] = nums[i] * 2
+  }
+
+  resp.send('Os dobros dos numeros: ' + nums2)
 })
  
 servidor.listen(5050, () => console.log("Subiu"))
